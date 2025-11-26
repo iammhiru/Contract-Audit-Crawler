baf5141c7bbd81bc5b44e5784d9d659fde7d5f3c
Pham Hieu <hieuphamsaga@gmail.com>
2025-11-26 11:11:14 +0700

    crawl v1

--- audit_crawler.py
+++ audit_crawler.py
@@ -1,8 +1,10 @@
+import uuid
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
+import html2text
 
 BASE_URL = "https://solodit.cyfrin.io/?b=false&f=&ff=&i=HIGH%2CMEDIUM%2CLOW%2CGAS&l=&maxf=&minf=&p={page}&pc=&pn=&qs=1&r=true&rf=alltime&rs=1&s=&sd=Desc&sf=Recency&t=&u=&ur=true"
 
@@ -10,7 +12,6 @@ BASE_URL = "https://solodit.cyfrin.io/?b=false&f=&ff=&i=HIGH%2CMEDIUM%2CLOW%2CGA
 def close_popup_if_exists(driver):
     try:
         close_button = driver.find_elements(By.XPATH, "//button[@title='Close']")
-        print(len(close_button))
         if close_button:
             close_button[-1].click()
         time.sleep(2)
@@ -30,7 +31,7 @@ def crawl_issue_list(driver, page_number, max_issue=10):
             EC.presence_of_element_located((By.CSS_SELECTOR, "a[href^='/issues/']"))
         )
     except:
-        print(f"⚠️ Page {page_number} không có dữ liệu!")
+        print(f"Page {page_number} không có dữ liệu!")
         return []
 
     time.sleep(1.0)
@@ -62,4 +63,109 @@ def crawl_issue_list(driver, page_number, max_issue=10):
 
 
 def crawl_issue_detail(driver, url):
-    return None
+    driver.get(url)
+    time.sleep(3.5)
+
+    data = {}
+
+    issue_id = driver.find_elements(By.XPATH, ".//li/span[contains(text(),'#')]")
+    if issue_id:
+        data["id"] = issue_id[0].text.strip()
+
+    header = driver.find_element(By.XPATH, "//h1/ancestor::div[2]")
+    if header:
+        title = header.find_element(By.XPATH, "//h1")
+        if title:
+            data["title"] = title.text.strip()
+
+        name_and_time = header.find_element(By.XPATH, ".//span")
+        if name_and_time:
+            nt = name_and_time.find_elements(By.XPATH, ".//span")
+            if len(nt) >= 3:
+                data["name"] = nt[0].text.strip()
+                data["time"] = nt[-1].text.strip()
+
+    overview = driver.find_elements(By.XPATH, "//span[contains(text(),'Overview')]")
+    if overview:
+        overview = overview[-1]
+        desc = overview.find_element(By.XPATH, "../../div[2]")
+
+        impact = desc.find_element(By.XPATH, ".//div[contains(text(),'Impact')]/..")
+        if impact:
+            impact_value = impact.find_element(By.XPATH, ".//p")
+            data["impact"] = impact_value.text.strip()
+
+        quality = desc.find_element(By.XPATH, ".//div[contains(text(),'Quality')]/..")
+        if quality:
+            quality_value = quality.find_element(By.XPATH, ".//span").text.strip().split()
+            data["quality_rate"] = quality_value[0]
+            data["quality_votes"] = quality_value[-1].strip("()")
+
+        rarity = desc.find_element(By.XPATH, ".//div[contains(text(),'Rarity')]/..")
+        if rarity:
+            rarity_value = rarity.find_element(By.XPATH, ".//span").text.strip().split()
+            data["rarity_rate"] = rarity_value[0]
+            data["rarity_votes"] = rarity_value[-1].strip("()")
+
+        full_report = desc.find_element(By.XPATH, ".//div[contains(text(),'Full report')]/..")
+        if full_report:
+            full_report_url = full_report.find_element(By.XPATH, ".//a")
+            data["full_report_url"] = full_report_url.get_attribute("href").strip()
+
+        categories = desc.find_element(By.XPATH, ".//div[contains(text(),'Categories')]/..")
+        if categories:
+            data["categories"] = categories.text.strip()
+
+        tags = desc.find_element(By.XPATH, ".//div[contains(text(),'Tags')]/..")
+        if tags:
+            data["tags"] = tags.text.strip()
+
+        authors = desc.find_element(By.XPATH, ".//div[contains(text(),'Author(s)')]/..")
+        if authors:
+            author_list = authors.find_element(By.XPATH, ".//span")
+            data["authors"] = author_list.text.strip().split()
+
+    wait = WebDriverWait(driver, 5)
+    markdown_block = wait.until(
+        EC.presence_of_element_located((By.XPATH, "//div[contains(@class,'markdown')]//p"))
+    )
+
+    description = driver.find_element(By.XPATH, "//div[contains(@class, 'markdown')]")
+    if description:
+        html_content = description.get_attribute("innerHTML")
+
+        soup = BeautifulSoup(html_content, "html.parser")
+        code_blocks = {}
+        placeholder_prefix = "CODEBLOCKPLACEHOLDER_"
+
+        for container in soup.find_all("div", class_="ql-code-block-container"):
+            language = container.get("data-language", "").strip()
+            code_lines = [
+                line.get_text() for line in container.find_all("div", class_="ql-code-block")
+            ]
+
+            markdown_code = "```" + language + "\n" + "\n".join(code_lines) + "\n```\n"
+
+            token = placeholder_prefix + str(uuid.uuid4()).replace("-", "")
+            code_blocks[token] = markdown_code
+            container.replace_with(token)
+
+        h = html2text.HTML2Text()
+        h.body_width = 0
+        h.single_line_break = True
+        h.ignore_links = False
+        h.ignore_images = True
+
+        md = h.handle(str(soup))
+
+        md = md.replace("\n**", "\n\n**")
+
+        for token, code in code_blocks.items():
+            md = md.replace(token, code)
+
+        md = md.replace("\\clearpage", "").strip()
+
+        data["description"] = md
+
+    return data
+
--- main.py
+++ main.py
@@ -2,12 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from audit_crawler import crawl_issue_list, crawl_issue_detail
+import json, os, re
+from datetime import datetime
+
+def sanitize_filename(name: str) -> str:
+    return re.sub(r'[\\/*?:"<>|]', '', name)
+
+def save_json(data: dict, title: str, folder="crawl"):
+    os.makedirs(folder, exist_ok=True)
+
+    title_clean = sanitize_filename(title)
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    filename = f"{title_clean}_{timestamp}.json"
+    filepath = os.path.join(folder, filename)
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    print("Saved result to:", filepath)
 
 
 def create_driver():
     options = webdriver.ChromeOptions()
 
     # options.add_argument("--headless")
+    # options.add_argument("--user-data-dir=/home/hieu/.config/google-chrome")
+    # options.add_argument("--profile-directory=SoloditProfile")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
@@ -36,11 +59,12 @@ if __name__ == "__main__":
     driver = create_driver()
 
     try:
-        issues = crawl_multiple_pages(driver, total_pages=2, max_issue_per_page=10)
+        issues = crawl_multiple_pages(driver, total_pages=1, max_issue_per_page=2)
 
-        print(f"\n✨ Crawl xong {len(issues)} issues.")
         for item in issues:
-            print(item)
-
-    except:
-        print("Đã có lỗi xảy ra trong quá trình crawl.")
+            print("==============================")
+            print("Crawling detail for issue:", item["title"])
+            result = crawl_issue_detail(driver, item["link"])
+            save_json(result, item["title"])
+    except Exception as e:
+        print("Đã có lỗi xảy ra trong quá trình crawl. Chi tiết:", str(e))
